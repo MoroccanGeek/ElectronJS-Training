c58512811d0a45b2a7467a88018937181b618888
Hamza SABER <saberhamza3@gmail.com>
2020-09-15 13:42:10 +0100

    Project Bookmarks-Saver: Initial Setup

---
+++ 4-master-electron/main.js
@@ -0,0 +1,56 @@
+// Modules
+const {app, BrowserWindow} = require('electron')
+const windowStateKeeper = require('electron-window-state')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+// Create a new BrowserWindow when `app` is ready
+function createWindow () {
+
+  // load the previous state with fallback to defaults
+  let mainWindowState = windowStateKeeper({
+    defaultWidth: 500,
+    defaultHeight: 650
+  })
+
+  mainWindow = new BrowserWindow({
+    x: mainWindowState.x,
+    y: mainWindowState.y,
+    width: mainWindowState.defaultWidth, 
+    height: mainWindowState.defaultHeight,
+    minWidth: 350,
+    minHeight: 300,
+    webPreferences: { 
+      nodeIntegration: true
+    }
+  })
+
+  // Load index.html into the new BrowserWindow
+  mainWindow.loadFile('renderer/main.html')
+
+  // Manage new window state
+  mainWindowState.manage(mainWindow);
+
+  // Open DevTools - Remove for PRODUCTION!
+  mainWindow.webContents.openDevTools();
+
+  // Listen for window being closed
+  mainWindow.on('closed',  () => {
+    mainWindow = null
+  })
+}
+
+// Electron `app` is ready
+app.on('ready', createWindow)
+
+// Quit when all windows are closed - (Not macOS - Darwin)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// When app icon is clicked and app is running, (macOS) recreate the BrowserWindow
+app.on('activate', () => {
+  if (mainWindow === null) createWindow()
+})
---
+++ 4-master-electron/renderer/app.js
@@ -0,0 +1,16 @@
+
+
+let showModal = document.getElementById('show-modal'),
+    closeModal = document.getElementById('close-modal'),
+    modal = document.getElementById('modal');
+
+// show Modal
+showModal.addEventListener('click', e => {
+    modal.style.display = 'flex'
+})
+
+// close Modal
+closeModal.addEventListener('click', e => {
+    modal.style.display = 'none'
+})
+
