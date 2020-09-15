72ffd2cf75ef15f55a92edaac509f9f9afc27f0e
Hamza SABER <saberhamza3@gmail.com>
2020-09-15 15:50:44 +0100

    Bookmarks-Saver: Submitting new items

--- 4-master-electron/renderer/app.js
+++ 4-master-electron/renderer/app.js
@@ -1,15 +1,69 @@
-
+const electron = require('electron')
+const {ipcRenderer} = electron
 
 let showModal = document.getElementById('show-modal'),
-closeModal = document.getElementById('close-modal'),
-modal = document.getElementById('modal');
+    closeModal = document.getElementById('close-modal'),
+    modal = document.getElementById('modal')
+    addItem = document.getElementById('add-item')
+    itemUrl = document.getElementById('url');
+
+
+// Toggle modal buttons
+const toggleModalButtons = () => {
+    // check state of buttons
+    if(addItem.disabled === true) {
+        addItem.disabled = false
+        addItem.style.opacity = 1
+        addItem.innerText = 'Add Item'
+        closeModal.style.display = 'inline'
+    }else {
+        addItem.disabled = true
+        addItem.style.opacity = 0.5
+        addItem.innerText = 'Adding...'
+        closeModal.style.display = 'none'
+    }
+}
 
 // show Modal
 showModal.addEventListener('click', e => {
-modal.style.display = 'flex'
+    modal.style.display = 'flex'
+    itemUrl.focus()
 })
 
 // close Modal
 closeModal.addEventListener('click', e => {
-modal.style.display = 'none'
+    modal.style.display = 'none'
+})
+
+// Handle new Items
+addItem.addEventListener('click', e => {
+
+    // Check a URL exists
+    if(itemUrl.value){
+        
+        // send new item to main process
+        ipcRenderer.send('newItem-channel', itemUrl.value);
+        
+        // Disable buttons
+        toggleModalButtons();
+    }
+})
+
+// Handle new-item response from Main
+ipcRenderer.on('new-item-success', (e, newItem) => {
+    console.log(newItem);
+    
+    // Active buttons
+    toggleModalButtons();
+
+    // Hide modal and clear value
+    itemUrl.value = ''
+    closeModal.click()
+})
+
+// Listen to keyboard submit
+itemUrl.addEventListener('keyup', e=> {
+    if(e.key === 'Enter'){
+        addItem.click()
+    }
 })
